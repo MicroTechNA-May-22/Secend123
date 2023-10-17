8349f6be3d2e17bbd887c34768da8f256dde7682
unknown <Mamun8122>
2023-10-17 14:45:37 -0500

    Remove BD

--- src/test/java/base/Base2.java
+++ src/test/java/base/Base2.java
@@ -3,7 +3,6 @@ package base;
 public class Base2 {
     public static void main(String[] args) {
         System.out.println("KD");
-        System.out.println("ED");
         System.out.println("PK");
         System.out.println("Mamun");
     }
