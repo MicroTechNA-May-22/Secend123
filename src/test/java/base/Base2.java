8e2c62d11bd3c65c50568ab1d13f0f79732bc7b2
unknown <Mamun8122>
2023-10-17 15:51:56 -0500

    srilanka added

--- src/test/java/base/Base2.java
+++ src/test/java/base/Base2.java
@@ -3,7 +3,6 @@ package base;
 public class Base2 {
     public static void main(String[] args) {
         System.out.println("Bangladesh");
-        System.out.println("Indea");
         System.out.println("Afganistan");
         System.out.println("Arab");
         System.out.println("Palastain");
