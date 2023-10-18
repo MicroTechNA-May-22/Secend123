400fc46834abf768197e1175ef6cfa38554bed7f
unknown <Mamun8122>
2023-10-17 20:47:38 -0500

    Danmoni added

--- src/test/java/base/Base2.java
+++ src/test/java/base/Base2.java
@@ -16,5 +16,6 @@ public class Base2 {
         System.out.println("Ogbakistan");
         System.out.println("Koria");
         System.out.println("bismilla");
+        System.out.println("Danmodi");
     }
 }
