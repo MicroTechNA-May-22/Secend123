ef89a26b90c4e9e4872ea5c19924d71251d9941f
unknown <Mamun8122>
2023-10-17 20:58:48 -0500

    Multpication done

--- src/test/java/base/Base2.java
+++ src/test/java/base/Base2.java
@@ -7,16 +7,20 @@ public class Base2 {
         System.out.println("Arab");
         int a=1;
         int b=2;
+        int d=5;
         int c = a+b;
         System.out.println(c);
         System.out.println("Palastain");
         System.out.println("London");
         System.out.println("Pakistan");
         System.out.println("Srilonka");
+        int e= a*b*c;
+        System.out.println(e);
         System.out.println("Commilla");
         System.out.println("Ogbakistan");
         System.out.println("Koria");
         System.out.println("bismilla");
         System.out.println("Danmodi");
+
     }
 }
