f6ecf58310dd1dfb52dcfb9bd8c82f0990b74786
unknown <Mamun8122>
2023-10-17 20:54:10 -0500

    Sum done

--- src/test/java/base/Base2.java
+++ src/test/java/base/Base2.java
@@ -7,6 +7,8 @@ public class Base2 {
         System.out.println("Arab");
         int a=1;
         int b=2;
+        int c = a+b;
+        System.out.println(c);
         System.out.println("Palastain");
         System.out.println("London");
         System.out.println("Pakistan");
