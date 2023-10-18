49c342b4dcb9762ab64544a126f41a4f96268d3f
Mamun8122 <95468221+Mamun8122@users.noreply.github.com>
2023-10-17 20:56:09 -0500

    Update Base2.java

--- src/test/java/base/Base2.java
+++ src/test/java/base/Base2.java
@@ -2,9 +2,7 @@ package base;
 
 public class Base2 {
     public static void main(String[] args) {
-        System.out.println("Dhaka");
-        System.out.println("Bangladesh");
-        System.out.println("Arab");
+
         int a=1;
         int b=2;
         int c = a+b;
